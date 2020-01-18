9ed5e768ea204db54537d37781715a23b92703aa
MohamedTebba <36170297+MohamedTebba@users.noreply.github.com>
2020-01-18 22:45:22 +0100

    modelization

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+const domElements = () => {
+
+    const domStrings = {
+        SECTION:'section',
+        NAVBAR_LIST:'navbar__list',
+        TO_UP:'.to-up',
+        PAGE_HEADER:'.page__header',
+    }
+    
+    return {
+        sections: document.querySelectorAll(domStrings.SECTION),
+        ulList: document.getElementById(domStrings.NAVBAR_LIST),
+        toUp:document.querySelector(domStrings.TO_UP),
+        header:document.querySelector(domStrings.PAGE_HEADER)
+
+    }
+
+}
+
+const createNavBar = (sections) => {
+    const fragment = document.createDocumentFragment();
+    Array.from(sections).forEach(e => {
+        const li = document.createElement('li');
+        li.setAttribute('class',e.id);
+        const a = document.createElement('a');
+        a.setAttribute('href',`#${e.id}`);
+        a.setAttribute('class','menu__link');
+        const sectionName = document.createTextNode(e.dataset.nav);
+        a.appendChild(sectionName);
+        li.appendChild(a);
+        fragment.appendChild(li);
+    })
+    
+    return fragment;
+}
+
+const isInViewPort = (element) => {
+    const scrollTop = pageYOffset;
+    const scrollBottom = scrollTop+innerHeight;
+    const offsetTop = element.offsetTop;
+    const offsetBottom = element.offsetHeight+element.offsetTop;
+    return ( offsetTop >= (scrollTop - innerHeight*40/100) && offsetBottom <= (scrollBottom + innerHeight*40/100)) ? true :false
+}
+
+
 /**
  * 
  * Manipulating the DOM exercise.
@@ -55,3 +100,54 @@
 // Set sections as active
 
 
+(function run(domElements,createNavBar,isInViewPort){
+    
+    domElements().ulList.appendChild(createNavBar(domElements().sections));
+
+    let isScrolling;
+
+    addEventListener('scroll',()=>{
+        Array.from(document.querySelectorAll('ul li')).forEach(li=>
+            li.classList.remove('menu__active-link')
+            )
+        
+        
+        Array.from(domElements().sections).forEach( s => {
+
+            s.classList.remove('your-active-class');
+
+                if(isInViewPort(s)){
+                s.classList.add('your-active-class');
+                Array.from(document.querySelectorAll('ul li')).forEach(li=>
+                    li.classList.remove('menu__active-link')
+                    )
+                document.querySelector(`.${s.id}`).classList.add('menu__active-link');
+            }
+            
+        })
+
+        clearTimeout(isScrolling);
+        domElements().header.classList.remove('hide-navbar');
+
+            if( pageYOffset > innerHeight ){
+        
+                domElements().toUp.classList.add('display-up');
+                isScrolling = setTimeout(()=>{
+                    domElements().header.classList.add('hide-navbar');
+                },2000);
+
+
+            }else{ 
+                domElements().toUp.classList.remove('display-up');
+                
+            }
+    })
+
+    domElements().toUp.addEventListener('click',()=>{
+      scrollTo(0,0);
+    })
+
+
+
+
+})(domElements,createNavBar,isInViewPort)
